421bcae287867804a94c6ef0c3f64880f33f2160
Bekir Çelik <bekircelik5858@gmail.com>
2020-11-28 10:43:47 +0300

    Add project files.

---
+++ RabbitMQConsumer/DirectExchangeConsumer.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    public static class DirectExchangeConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.ExchangeDeclare("demo-direct-exchange", ExchangeType.Direct);
+            channel.QueueDeclare("demo-direct-queue",
+               durable: true,
+               exclusive: false,
+               autoDelete: false,
+               arguments: null);
+            channel.QueueBind("demo-direct-queue", "demo-direct-exchange", "account.init");
+            channel.BasicQos(0, 10, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-direct-queue", true, consumer);
+            Console.WriteLine("Consume started");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5673")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            //QueueConsumer.Consume(channel);
+            //DirectExchangeConsumer.Consume(channel);
+            //TopicExchangeConsumer.Consume(channel);
+            //HeaderExchangeConsumer.Consume(channel);
+            FanoutExchangeConsumer.Consume(channel);
+        }
+    }
+}
